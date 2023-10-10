90470a2fb225790065f37bfbfbc301a6a2c7c997
alim-2023 <alim@crystallitedigital.com>
2023-10-10 19:12:44 +0500

    pixel

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ import Footernewfy from '@/components/Footernewfydesign';
 import Zendesk, { ZendeskAPI } from "../pages/zendex";
 const ZENDESK_KEY = "325da280-f4f0-4c80-997f-ea4de45eb2f1";
 import Script from 'next/script';
-import Pixel from '@/components/Pixel';
+import Pixel from '@/components/Pixel'; // Import the Pixel component
 
 export default function App({ Component, pageProps }) {
 
@@ -20,6 +20,21 @@ export default function App({ Component, pageProps }) {
   gtag('js', new Date());
   gtag('config', 'AW-11331242978');`;
 
+  const mod = `https://www.facebook.com/tr?id=850012750160683&ev=PageView&noscript=1`
+  const mod1 = `
+  !function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '850012750160683');
+fbq('track', 'PageView');
+  
+  `
+
   return (
     <>
 
@@ -28,6 +43,16 @@ export default function App({ Component, pageProps }) {
       <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(gt1) }}></script>
       <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(gt2) }}></script>
 
+      <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(mod) }}></script>
+      <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(mod1) }}></script>
+
+      {/* <Script async src="https://www.googletagmanager.com/gtag/js?id=AW-11331242978"></Script>
+      <Script>
+        {`window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments)};
+                gtag('js', new Date());
+                gtag('config', 'AW-11331242978');`}
+      </Script> */}
       <Pixel />
 
       <Component {...pageProps} />
