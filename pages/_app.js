481c44c125d283926af6453e2b0200d82abdd0e4
alim-2023 <alim@crystallitedigital.com>
2023-10-10 19:17:26 +0500

    pixel2

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,7 @@ import Zendesk, { ZendeskAPI } from "../pages/zendex";
 const ZENDESK_KEY = "325da280-f4f0-4c80-997f-ea4de45eb2f1";
 import Script from 'next/script';
 import Pixel from '@/components/Pixel'; // Import the Pixel component
+import Pixel2 from '@/components/Pixel2';
 
 export default function App({ Component, pageProps }) {
 
@@ -20,20 +21,8 @@ export default function App({ Component, pageProps }) {
   gtag('js', new Date());
   gtag('config', 'AW-11331242978');`;
 
-  const mod = `https://www.facebook.com/tr?id=850012750160683&ev=PageView&noscript=1`
-  const mod1 = `
-  !function(f,b,e,v,n,t,s)
-{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window, document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '850012750160683');
-fbq('track', 'PageView');
+
   
-  `
 
   return (
     <>
@@ -43,8 +32,7 @@ fbq('track', 'PageView');
       <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(gt1) }}></script>
       <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(gt2) }}></script>
 
-      <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(mod) }}></script>
-      <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(mod1) }}></script>
+     
 
       {/* <Script async src="https://www.googletagmanager.com/gtag/js?id=AW-11331242978"></Script>
       <Script>
@@ -54,6 +42,7 @@ fbq('track', 'PageView');
                 gtag('config', 'AW-11331242978');`}
       </Script> */}
       <Pixel />
+      <Pixel2 />
 
       <Component {...pageProps} />
 
